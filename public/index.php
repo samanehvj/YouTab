4a1fd9d7f1ac89e4393ffb5565b0c1e72cfb546d
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-03 12:27:56 -0800

    Define variables in index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,12 @@
-<h1>Hello</h1>
+<?php
+
+define('URLROOT', 'http://localhost:8888/YouTab');
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
+define('VIEW', APP . 'views' . DIRECTORY_SEPARATOR);
+define('MODEL', APP . 'models' . DIRECTORY_SEPARATOR);
+define('CONTROLLER', APP . 'controllers' . DIRECTORY_SEPARATOR);
+define('LIB', APP . 'libs' . DIRECTORY_SEPARATOR);
+define('SITENAME', 'YouTab Brand');
+
+
