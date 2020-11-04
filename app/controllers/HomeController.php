6a607616c626575032b27ee3c15bd7f1087f19dc
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-04 00:59:46 -0800

    Create HomeController

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+class HomeController {
+
+    public function index() {
+        echo "I am in class: " . __CLASS__ . " calling method: " . __METHOD__;
+    }
+    
+    public function about() {
+        echo "I am in class: " . __CLASS__ . " calling method: " . __METHOD__;
+    }
+}
