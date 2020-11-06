5c6756c44d8df95150a12167bd03b1c9f68d8b8a
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-06 08:04:47 -0800

    Create a simple products view page

---
+++ app/views/product/products.php
@@ -0,0 +1,9 @@
+<?php include VIEW . 'layouts/header.php' ?>
+
+    <h1>Welcome to YouTab Products Page</h1>
+    <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+    </ul>
+
+<?php include VIEW . 'layouts/footer.php' ?>
