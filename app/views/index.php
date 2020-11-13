0645d576c339602e4991df7803eb853f29664dbe
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-13 00:30:43 -0800

    ad hero image to index page

--- app/views/index.php
+++ app/views/index.php
@@ -1,5 +1,9 @@
 <?php include VIEW . 'layouts/header.php' ?>
 
+
+    <div class="container-fluid ">
+        <img src="/imgs/hero-home.jpg" class="img-fluid w-100 ">
+    </div>
 <h1>Welcome to YouTab Home Page</h1>
 <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iure tenetur facere, sed inventore voluptates distinctio, atque, quisquam alias deleniti porro quidem praesentium eum dolor ipsum nam? Explicabo, aliquam omnis.
