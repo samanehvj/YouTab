99fe434839be2bbfbf35c0cfc6d8a5724a616521
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-13 00:29:57 -0800

    Edit about page to use new layout

--- app/views/about.php
+++ app/views/about.php
@@ -1,8 +1,41 @@
 <?php include VIEW . 'layouts/header.php' ?>
 
-<h1>YouTab Bio</h1>
-<p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis ut eius id repellat reprehenderit aperiam tempore quae doloribus fugiat asperiores nesciunt dignissimos nam, soluta voluptates fuga corporis enim. Dignissimos, ab?
-</p>
+<!--start hero image -->
+    <div class="container-fluid ">
+        <img src="/imgs/hero-contact.jpg" class="img-fluid w-100 ">
+    </div> <!--end hero image -->
 
+<!--    <div class="card bg-light my-5 mx-5" style="max-width: 40rem">-->
+<!--        <div class="card-header">About YouTab</div>-->
+<!--        <div class="card-body">-->
+<!--            <h5 class="card-title">Light card title</h5>-->
+<!--            <p class="card-text">Influential, innovative and progressive, YOUTAB is reinventing a wholly modern approach to fashion. Under the new vision of creative director Alessandro Michele, the House has redefined luxury for the 21st century, further reinforcing its position as one of the world’s most desirable fashion houses. Eclectic, contemporary, romantic—YOUTAB products represent the pinnacle of Italian craftsmanship and are unsurpassed for their quality and attention to detail.-->
+<!--                <br>-->
+<!--                <br>-->
+<!--                <br>-->
+<!---->
+<!--                YOUTAB is part of the Kering Group. A global Luxury group, Kering manages the development of a series of renowned Houses in Fashion, Leather Goods.-->
+<!---->
+<!--        </div>-->
+<!--    </div>-->
+    <div class="container ">
+        <div class="row">
+        <h1 class="m-3 p-2 text-center col-12">About YouTab</h1>
+            <div class="col-lg-8">
+        <p class="p-4 mb-2">
+            Influential, innovative and progressive, YOUTAB is reinventing a wholly modern approach to fashion. Under the new vision of creative director Alessandro Michele, the House has redefined luxury for the 21st century, further reinforcing its position as one of the world’s most desirable fashion houses. Eclectic, contemporary, romantic—YOUTAB products represent the pinnacle of Italian craftsmanship and are unsurpassed for their quality and attention to detail.
+        </p>
+        <p class="p-4">
+            YOUTAB is part of the Kering Group. A global Luxury group, Kering manages the development of a series of renowned Houses in Fashion, Leather Goods.
+        </p>
+            </div>
+            <div class="col-lg-4">
+                <img src="/imgs/youtab-image.png" class="img-fluid col-12">
+                <p class="col-12">
+                    This prinsace was very good
+                </p>
+            </div>
+
+        </div>
+    </div>
 <?php include VIEW . 'layouts/footer.php' ?>
