55f6d297de9374226f6cfe6df5fbb94c36eb1b5d
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-14 02:42:40 -0800

    Edit margin in about heading

--- app/views/about.php
+++ app/views/about.php
@@ -19,10 +19,10 @@
 <!--        </div>-->
 <!--    </div>-->
     <div class="container ">
-        <div class="row">
-        <h1 class="m-3 p-2 text-center col-12">About YouTab</h1>
-            <div class="col-lg-8">
-        <p class="p-4 mb-2">
+        <div class="row ">
+        <h1 class="m-3 p-2 text-center col-12 mt-5">About YouTab</h1>
+            <div class="col-lg-8 about-text">
+        <p class="p-4 mb-2 ">
             Influential, innovative and progressive, YOUTAB is reinventing a wholly modern approach to fashion. Under the new vision of creative director Alessandro Michele, the House has redefined luxury for the 21st century, further reinforcing its position as one of the world’s most desirable fashion houses. Eclectic, contemporary, romantic—YOUTAB products represent the pinnacle of Italian craftsmanship and are unsurpassed for their quality and attention to detail.
         </p>
         <p class="p-4">
@@ -31,9 +31,8 @@
             </div>
             <div class="col-lg-4">
                 <img src="/imgs/youtab-image.png" class="img-fluid col-12">
-                <p class="col-12">
-                    This prinsace was very good
-                </p>
+                <p class="col-12 my-4 image-text">
+                    Youtab was a legendary ancient Persian noblewoman. She was the sister of Ariobarzanes, Satrap of Persis. She is notable for fighting alongside her brother against Macedonian King Alexander the Great at the Battle of the Persian Gate in the winter of 330 BC.                </p>
             </div>
 
         </div>
