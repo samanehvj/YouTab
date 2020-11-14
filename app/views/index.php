c1e89f3ed2238b7e4a5ba0619e7bc8983473343d
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-14 02:43:21 -0800

    Update index page to show categories and top products

--- app/views/index.php
+++ app/views/index.php
@@ -4,9 +4,109 @@
     <div class="container-fluid ">
         <img src="/imgs/hero-home.jpg" class="img-fluid w-100 ">
     </div>
-<h1>Welcome to YouTab Home Page</h1>
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iure tenetur facere, sed inventore voluptates distinctio, atque, quisquam alias deleniti porro quidem praesentium eum dolor ipsum nam? Explicabo, aliquam omnis.
-</p>
+<!--start container-->
+    <div  class="container">
+        <div class="row my-5"> <!--row -->
+            <div class=" col-lg-6"> <!--start left cat-->
+                <div class="larg-cat d-flex flex-column justify-content-between h-100 mb-3 mb-lg-0 p-2 border border-dark">
+                    <img class="img-fluid w-100" src="/imgs/skirt-cat.jpg">
+                    <h1 class="text-center my-3">Skirts</h1>
+                    <p class="py-3 px-5 text-center text-lg-left text-cat">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.                     </p>
+                    <div class="text-center mt-5">
+                        <a href="products/category/1" class="btn btn-dark text-center collection">View collection </a>
+                    </div>
+                </div>
+            </div> <!--end left cat-->
+            <div class=" col-lg-6 d-flex flex-column justify-content-between">  <!--start right cat-->
+                <div class="col-12 small-cat mb-4 p-2 border border-dark">
+                    <img class="img-fluid w-100" src="/imgs/dress-cat.jpg">
+                    <h1 class="text-center my-3">Dresses</h1>
+                    <div class="text-center">
+                        <a href="products/category/1" class="btn btn-dark text-center">View collection </a>
+                    </div>
+                </div>
+                <div class="col-12 small-cat p-2 border border-dark">
+                    <img class="img-fluid w-100" src="/imgs/top-cat.jpg">
+                    <h1 class="text-center my-3">Tops</h1>
+                    <div class="text-center btn-cat">
+                        <a href="products/category/1" class="btn btn-dark text-center">View collection </a>
+                    </div>
+                </div>
+            </div> <!--end right-top cat-->
+        </div> <!--end of  row-->
+
+
+
+
+        <h1 class="text-center my-5"> Most popular products in YouTab </h1>
+
+            <div class="row my-5"> <!-- start row of Most famous product-->
+                <div class=" col-lg-6"> <!-- start row of dress left side  famous product-->
+                    <div class="larg-cat d-flex flex-column justify-content-between h-100 mb-3 mb-lg-0 p-2  ">
+                        <img class="img-fluid w-100" src="/imgs/dresses/4.png">
+                    </div>
+                </div> <!-- end row of dress left side  famous product-->
+
+                <div class="col-lg-6 row">
+
+                    <div class=" col-lg-6 ">  <!--start right cat-->
+                        <div class="col-12 small-cat mb-4 p-2 k">
+                            <img class="img-fluid w-100" src="/imgs/skirts/18.png">
+                            <h5 class="products-text mx-4 my-2">
+                                Skirt with Metallic Buckle
+                            </h5>
+                            <h5 class="mx-4 my-2">
+                                $2000
+                            </h5>
+                        </div>
+                    </div>
+
+
+                    <div class=" col-lg-6 ">  <!--start right cat-->
+                        <div class="col-12 small-cat mb-4 p-2 k">
+                            <img class="img-fluid w-100" src="/imgs/tops/4.png">
+                            <h5 class="products-text mx-4 my-2">
+                                Sleeveless Shirt
+                            </h5>
+                            <h5 class="mx-4 my-2">
+                                $2000
+                            </h5>
+                        </div>
+                    </div>
+
+
+
+                    <div class=" col-lg-6 ">  <!--start right cat-->
+                        <div class="col-12 small-cat mb-4 p-2 k">
+                            <img class="img-fluid w-100" src="/imgs/tops/14.png">
+                            <h5 class="products-text mx-4 my-2">
+                                Short-Sleeeved T-Shirt                            </h5>
+                            <h5 class="mx-4 my-2">
+                                $2000
+                            </h5>
+                        </div>
+                    </div>
+
+
+                    <div class=" col-lg-6 ">  <!--start right cat-->
+                        <div class="col-12 small-cat mb-4 p-2 k">
+                            <img class="img-fluid w-100" src="/imgs/skirts/4.jpg">
+                            <h5 class="products-text mx-4 my-2">
+                                A-Line Mini Skirt                             </h5>
+                            <h5 class="mx-4 my-2">
+                                $2000
+                            </h5>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div> <!--end container-->
+
+
+
 
 <?php include VIEW . 'layouts/footer.php' ?>
