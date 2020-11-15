425299e9b56c9e741dbe972454a3a3344da9885f
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-14 17:23:08 -0800

    Change product page location

---
+++ app/views/products.php
@@ -0,0 +1,165 @@
+<?php include VIEW . 'layouts/header.php' ?>
+
+   <div class="container"> <!--start container-->
+
+       <div class="row my-5 product-title"> <!--start row-->
+
+           <!-- start row 1  dress-->
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
+               <img src="/imgs/dresses/8.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+               <div class="row">
+                   <div class="col-7">
+                       <a href="">Fit-And-Flare Dress </a><br/>
+                       <span>$2900</span>
+                   </div>
+                   <div class="col-4 my-2">
+                       <span class="py-2 px-3 ml-1 bg-light border border-dark"> </span>
+                       <span class="py-2 px-3 ml-1 bg-dark"> </span>
+                   </div>
+           </div>
+               </div>
+           </div>
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
+               <img src="/imgs/dresses/17.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7">
+                           <a href="">Floral Print Mini-Dress </a><br/>
+                           <span>$2900</span>
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
+               <img src="/imgs/dresses/4.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7">
+                           <a href="">Leather dress</a><br/>
+                           <span>$2900</span>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+           <!-- end row 1  dress-->
+
+
+
+
+           <!-- start row 2  top-->
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
+               <img src="/imgs/tops/2.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7">
+                           <a href="">Sleeveless Shirt </a><br/>
+                           <span>$2900</span>
+                       </div>
+                       <div class="col-4 my-2">
+                           <span class="py-2 px-3 ml-1 bg-light border border-dark"> </span>
+                           <span class="py-2 px-3 ml-1 bg-dark"> </span>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
+               <img src="/imgs/tops/12.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7">
+                           <a href="">Short-Sleeeved </a><br/>
+                           <span>$2900</span>
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
+               <img src="/imgs/tops/17.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7">
+                           <a href="">Side Strap T-Shirt </a><br/>
+                           <span>$2900</span>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+           <!-- end row 2  top-->
+
+
+
+
+           <!-- start row 3  skirt-->
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
+               <img src="/imgs/skirts/17.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7">
+                           <a href="">Skirt with Metallic </a><br/>
+                           <span>$2900</span>
+                       </div>
+                       <div class="col-4 my-2">
+                           <span class="py-2 px-3 ml-1 bg-danger"> </span>
+                           <span class="py-2 px-3 ml-1 bg-dark"> </span>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
+               <img src="/imgs/skirts/3.jpg" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7">
+                           <a href="">A-Line Mini Skirt </a><br/>
+                           <span>$2900</span>
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+
+
+           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
+               <img src="/imgs/skirts/7.png" class="img-fluid col-12">
+               <div class="col-12 mt-2 ">
+                   <div class="row">
+                       <div class="col-7  ">
+                           <a href="">Monogram Mini Skirt </a><br/>
+                           <span>$2900</span>
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+
+           <!-- end row 3  skirt-->
+
+
+       </div> <!--end row-->
+
+   </div> <!--end container-->
+
+
+<?php include VIEW . 'layouts/footer.php' ?>
