29619fcce7418d683f5be5931a29f8037f6aea29
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-18 03:09:56 -0800

    Add link to product img and title to product_detail from product view page

--- app/views/product/products.php
+++ app/views/product/products.php
@@ -7,11 +7,13 @@
            <!-- start row 1  dress-->
 
            <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
-               <img src="/imgs/dresses/8.png" class="img-fluid col-12">
+               <a href="/product/productDetail">
+                   <img src="/imgs/dresses/8.png" class="img-fluid col-12">
+               </a>
                <div class="col-12 mt-2 ">
                <div class="row">
                    <div class="col-7">
-                       <a href="">Fit-And-Flare Dress </a><br/>
+                       <a href="/product/productDetail">Fit-And-Flare Dress </a><br/>
                        <span>$2900</span>
                    </div>
                    <div class="col-4 my-2">
@@ -24,11 +26,13 @@
 
 
            <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <img src="/imgs/dresses/17.png" class="img-fluid col-12">
+               <a href="/product/productDetail">
+                   <img src="/imgs/dresses/17.png" class="img-fluid col-12">
+               </a>
                <div class="col-12 mt-2 ">
                    <div class="row">
                        <div class="col-7">
-                           <a href="">Floral Print Mini-Dress </a><br/>
+                           <a href="/product/productDetail">Floral Print Mini-Dress </a><br/>
                            <span>$2900</span>
                        </div>
 
@@ -39,11 +43,13 @@
 
 
            <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <img src="/imgs/dresses/4.png" class="img-fluid col-12">
+               <a href="/product/productDetail">
+                   <img src="/imgs/dresses/4.png" class="img-fluid col-12">
+               </a>
                <div class="col-12 mt-2 ">
                    <div class="row">
                        <div class="col-7">
-                           <a href="">Leather dress</a><br/>
+                           <a href="/product/productDetail">Leather dress</a><br/>
                            <span>$2900</span>
                        </div>
                    </div>
