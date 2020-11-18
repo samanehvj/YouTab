5e400a72f505e8cca0ff1a9ad9a204fe536c364a
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-18 03:08:30 -0800

    Create product_detail view page

---
+++ app/views/product/product_detail.php
@@ -0,0 +1,74 @@
+<?php include VIEW . 'layouts/header.php' ?>
+
+    <div class="container-fluid product-detail-page">
+
+        <div class="row my-5">
+
+
+            <div class="col-lg-2 order-2 order-lg-1 d-flex product-detail-thumbs ">
+                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
+                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
+                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
+                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
+                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
+
+            </div>
+            <div class=" col-lg-6 order-1 order-lg-">
+                <div class="larg-cat d-flex flex-column justify-content-between h-100 mb-3 mb-lg-0 p-2  ">
+                    <img class="img-fluid w-100" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656">
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 order-3 ">
+
+
+                <div class="card" >
+                    <div class="card-body">
+                        <h3 class="card-title">Leather Dress</h3>
+                        <h5>$3500</h5>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Size</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>X-Small</option>
+                                <option>Small</option>
+                                <option>Medium</option>
+                                <option>large</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Color</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Navy</option>
+                                <option>White</option>
+
+                            </select>
+                        </div>
+                        <p>Sleek and structured, this sleeveless fit-and-flare dress is cut from technical gabardine with tone-on-tone lining. Leather inserts create a sharp, graphic look and nod to YouTab heritage of craftsmanship. A statement zipper runs the length of the body and a jewel-like metallic buckle adds a statement feature.</p>
+
+
+                        <div class="text-center mt-5">
+                            <a href="products/category/1" class="btn btn-dark text-center w-50 "> Add To cart </a>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include VIEW . 'layouts/footer.php' ?>
