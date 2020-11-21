1deabceeba588e056a36871853f433e384dc8df6
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-21 00:42:10 -0800

    Create AdminController and dashboard action

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        if (isset($_SESSION['userIsAdmin']) && $_SESSION['userIsAdmin']) {
+//            $subscriberModel = $this->model('Subscriber');
+//            $dashboardData['subscriberCount'] = $subscriberModel->count();
+
+            $this->setView('admin/dashboard');
+            $this->view->render();
+        } else {
+            $this->go('home', 'auth');
+        }
+    }
+}
