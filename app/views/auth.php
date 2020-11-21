7e3e2405c1ce31ce882335ac2fb7f2ad7fd93fd5
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-21 00:50:16 -0800

    Update auth.pho in view

--- app/views/auth.php
+++ app/views/auth.php
@@ -7,19 +7,20 @@
             <div class="col-lg-5 bg login-text d-flex flex-column justify-content-around">
                 <h3 class="text-center ">LOGIN TO ACCOUNT</h3>
 
-                <form  >
+                <form method="post" action="/user/login" >
                     <div class="form-group mb-3">
-                        <input id="inputEmail" type="email" placeholder="Username" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
+                        <input name="email" id="inputEmail" type="email" placeholder="Email" required class="form-control rounded-pill border-0 shadow-sm px-4">
                     </div>
                     <div class="form-group mb-3">
-                        <input id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-danger"><br>
+                        <input name="password" id="inputPassword" type="password" placeholder="Password" required class="form-control rounded-pill border-0 shadow-sm px-4 text-danger"><br>
                     </div>
-                    <div class="custom-control custom-checkbox mb-3">
-                        <input id="customCheck1" type="checkbox" checked class="custom-control-input btn-dark">
-                        <label for="customCheck1" class="custom-control-label">Remember password</label>
-                    </div>
+
                     <button type="submit" class="btn btn-danger btn-block text-uppercase mb-2 rounded-pill shadow-s btn-dark text-light">Sign in
                     </button>
+                    <div class="custom-control custom-checkbox mb-3">
+                        <a href="#" >Forget Password</a>
+
+                    </div>
 
                 </form>
             </div>
@@ -28,18 +29,16 @@
             <div class=" col-lg-5 bg login-text d-flex flex-column justify-content-around mt-5 mt-lg-0">
                 <h3 class=" text-center">RESIGTER NOW</h3>
 
-                <form >
+                <form method="post" action="/user/register" >
                     <div class="form-group mb-3">
-                        <input id="FName" type="First Name" placeholder="First Name" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
+                        <input name="name" id="FName" type="text" placeholder="Name" required class="form-control rounded-pill border-0 shadow-sm px-4">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <input name="email" id="inputEmail" type="email" placeholder="Email" required class="form-control rounded-pill border-0 shadow-sm px-4">
                     </div>
                     <div class="form-group mb-3">
-                        <input id="lName" type="Last Name" placeholder="Last Name" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-danger"><br>
-                    </div>
-                    <div class="form-group mb-3">
-                        <input id="inputEmail" type="email" placeholder="Email" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
-                    </div>
-                    <div class="form-group mb-3">
-                        <input id="Password" type="Password" placeholder="Password" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
+                        <input name="password" id="Password" type="Password" placeholder="Password" required class="form-control rounded-pill border-0 shadow-sm px-4">
                     </div>
 
                     <button type="submit" class="btn btn-danger btn-block text-uppercase mb-2 rounded-pill shadow-s btn-dark text-light mb-4">Register
