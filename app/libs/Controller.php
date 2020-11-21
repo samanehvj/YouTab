c97ef059b6b2f539c92c9c17ea56c97d5543e2d6
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-21 00:45:57 -0800

    Update Controller to have go action and model action

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -4,9 +4,23 @@ class Controller
 {
     protected $view;
 
+    public function go($controller, $action)
+    {
+        $goingTo = "Location: " . URLROOT . "/" . $controller . "/" . $action;
+
+        header($goingTo);
+    }
+
     public function setView($viewFile, $viewData = []) 
     {
         $this->view = new View($viewFile, $viewData);
         return $this->view;
+    }
+
+    public function model($model)
+    {
+        $model = ucfirst($model);
+        require_once MODEL . $model . '.php';
+        return new $model;
     }
 }
