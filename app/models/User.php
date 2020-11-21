27e4c7252cb7ba7aea2e4881e5c0e9540d7d3b8b
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-21 02:13:09 -0800

    Add add action in User.php model

--- app/models/User.php
+++ app/models/User.php
@@ -21,4 +21,27 @@ class User
 
         return  $this->db->single();
     }
+
+    public function add($name, $email, $password, $address = '', $postal_code = '', $phone = '')
+    {
+        $sql = "INSERT INTO users 
+        (name, email, password, address, postal_code, phone) 
+        values 
+        (:name, :email, :password, :address, :postal_code, :phone)";
+
+        $this->db->query($sql);
+
+        $this->db->bind(':name', $name);
+        $this->db->bind(':email', $email);
+        $this->db->bind(':password', password_hash($password, PASSWORD_DEFAULT));
+        $this->db->bind(':address', $address);
+        $this->db->bind(':postal_code', $postal_code);
+        $this->db->bind(':phone', $phone);
+
+        if($this->db->execute()) {
+            return $this->getByEmail($email);
+        }
+
+        return false;
+    }
 }
