b2be01b66aaf4c4fd3c3172f4963b8471cda633e
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-23 15:25:00 -0800

    Update categoriesList.php to add edit and delete actions

--- app/views/admin/categoriesList.php
+++ app/views/admin/categoriesList.php
@@ -5,12 +5,14 @@ include VIEW . 'admin/layouts/sidebar.php';
 
 ?>
 <div class="table-responsive">
+    <a href="/category/adminAdd" class="btn btn-success m-5">Add Category</a>
     <table class="table table-striped table-sm">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Name</th>
                 <th>Image</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
@@ -19,6 +21,9 @@ include VIEW . 'admin/layouts/sidebar.php';
                     <td><?= $category->id ?></td>
                     <td><?= $category->name ?></td>
                     <td><img src="<?= URLROOT . '/' . $category->img ?>" width="50"> </td>
+                    <td>
+                        <a href="/category/adminEdit/<?= $category->id ?>" class="btn btn-warning">Edit</a>
+                        <a href="/category/adminDelete/<?= $category->id ?>" class="btn btn-danger">Delete</a> </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
