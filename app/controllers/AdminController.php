b5245733b2b619c036e7e50e68fb36752e14a032
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-23 15:19:30 -0800

    Remove comments in AdminController dashboard action

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -7,9 +7,6 @@ class AdminController extends Controller
     public function dashboard()
     {
         if (isset($_SESSION['userIsAdmin']) && $_SESSION['userIsAdmin']) {
-//            $subscriberModel = $this->model('Subscriber');
-//            $dashboardData['subscriberCount'] = $subscriberModel->count();
-
             $this->setView('admin/dashboard');
             $this->view->render();
         } else {
