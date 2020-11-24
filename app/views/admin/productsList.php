992950897a4e863b08ff282e4e979ae15c7be860
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-24 10:18:57 -0800

    Create productList php

---
+++ app/views/admin/productsList.php
@@ -0,0 +1,40 @@
+<?php
+
+include VIEW . 'admin/layouts/header.php';
+include VIEW . 'admin/layouts/sidebar.php';
+
+?>
+<div class="table-responsive">
+    <a href="/product/adminAdd" class="btn btn-success m-5">Add Product</a>
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Category</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>View</th>
+                <th>Date</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($this->viewData['products'] as $product) : ?>
+                <tr>
+                    <td><?= $product->id ?></td>
+                    <td><?= $product->category_id ?></td>
+                    <td><?= $product->name ?></td>
+                    <td>$<?= $product->price ?></td>
+                    <td><?= $product->view ?></td>
+                    <td><?= $product->date ?></td>
+                    <td>
+                        <a href="/product/adminEdit/<?= $product->id ?>" class="btn btn-warning">Edit</a>
+                        <a href="/product/adminDelete/<?= $product->id ?>" class="btn btn-danger">Delete</a> </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<?php
+
+include VIEW . 'admin/layouts/footer.php';
