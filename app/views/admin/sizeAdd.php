e0b6a32324e485fcae2dac6c41e7b5df1e8eda29
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-23 21:58:41 -0800

    Create sizeAdd.php

---
+++ app/views/admin/sizeAdd.php
@@ -0,0 +1,20 @@
+<?php
+
+include VIEW . 'admin/layouts/header.php';
+include VIEW . 'admin/layouts/sidebar.php';
+
+?>
+<div class="container d-flex justify-content-around ">
+    <form class="mt-5" method="post" action="/size/adminDoAdd" >
+        <div class="form-group">
+            <input class="form-control" id="name" name="name" placeholder="Size Name"/>
+        </div>
+
+
+        <button type="submit" class="btn btn-success mt-3">Save</button>
+
+    </form>
+</div>
+<?php
+
+include VIEW . 'admin/layouts/footer.php';
