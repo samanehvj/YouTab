2c71205c72e64886df9f083cb2be19e93eb4b91f
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-23 16:06:16 -0800

    Create colorEdit.php

---
+++ app/views/admin/colorEdit.php
@@ -0,0 +1,24 @@
+<?php
+
+include VIEW . 'admin/layouts/header.php';
+include VIEW . 'admin/layouts/sidebar.php';
+
+?>
+<div class="container d-flex justify-content-around ">
+    <form class="mt-5" method="post" action="/color/adminDoEdit" >
+        <input type="hidden" name="id" value="<?= $this->viewData->id ?>" />
+
+        <div class="form-group">
+            <input value="<?= $this->viewData->name ?>" class="form-control" id="name" name="name" placeholder="Color Name"/>
+        </div>
+        <div class="form-group">
+            <input value="<?= $this->viewData->hex ?>" class="form-control" id="hex" name="hex" placeholder="Color Hex (4F96A7)"/>
+        </div>
+        
+        <button type="submit" class="btn btn-warning mt-3">Edit</button>
+
+    </form>
+</div>
+<?php
+
+include VIEW . 'admin/layouts/footer.php';
