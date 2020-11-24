d17bcbceda95db2f0fda200ffc842fcfde250f88
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-23 21:58:57 -0800

    Create Size.php

---
+++ app/models/Size.php
@@ -0,0 +1,62 @@
+<?php
+
+Class Size {
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new DB;
+    }
+
+    public function getAll()
+    {
+        $sql = "SELECt * FROM sizes";
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getById($id)
+    {
+        $sql = "SELECt * FROM sizes WHERE id = :id";
+        $this->db->query($sql);
+        $this->db->bind(':id', $id);
+
+        return $this->db->single();
+    }
+
+    public function add($name)
+    {
+        $sql = "INSERT INTO sizes 
+        (name) 
+        values 
+        (:name)";
+
+        $this->db->query($sql);
+
+        $this->db->bind(':name', $name);
+
+        return $this->db->execute();
+    }
+
+    public function edit($id, $name)
+    {
+        $sql = "UPDATE sizes SET  name=:name WHERE id=:id";
+        $this->db->query($sql);
+        $this->db->bind(':name', $name);
+        $this->db->bind(':id', $id);
+
+        return $this->db->execute();
+    }
+
+    public function delete($id)
+    {
+        $sql = "DELETE FROM sizes WHERE id = :id";
+        $this->db->query($sql);
+        $this->db->bind(':id', $id);
+
+        return $this->db->execute();
+    }
+
+}
+?>
