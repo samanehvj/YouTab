11b0eae09d740b65fac3da26f94fa522a0da512a
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-23 16:06:13 -0800

    Create colorAdd.php

---
+++ app/views/admin/colorAdd.php
@@ -0,0 +1,22 @@
+<?php
+
+include VIEW . 'admin/layouts/header.php';
+include VIEW . 'admin/layouts/sidebar.php';
+
+?>
+<div class="container d-flex justify-content-around ">
+    <form class="mt-5" method="post" action="/color/adminDoAdd" >
+        <div class="form-group">
+            <input class="form-control" id="name" name="name" placeholder="Color Name"/>
+        </div>
+        <div class="form-group">
+            <input  class="form-control" id="hex" name="hex" placeholder="Hex Number (4FcategoryEdit.php36A9)"/>
+        </div>
+
+        <button type="submit" class="btn btn-success mt-3">Save</button>
+
+    </form>
+</div>
+<?php
+
+include VIEW . 'admin/layouts/footer.php';
