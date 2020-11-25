39a12a7f42bafdb9dcad21f7ff941143b9b2a2a2
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-24 22:37:26 -0800

    Create productColorSizeList.php

---
+++ app/views/admin/productColorSizesList.php
@@ -0,0 +1,42 @@
+<?php
+
+include VIEW . 'admin/layouts/header.php';
+include VIEW . 'admin/layouts/sidebar.php';
+
+?>
+<div class="table-responsive">
+    <h3 class="text-center my-3">
+        <?= $this->viewData['productColor']->product_name . " : (" . $this->viewData['productColor']->color_name .")" ?>
+    </h3>
+
+    <a href="/product/adminColorSizeAdd/<?= $this->viewData['productColor']->id ?>" class="btn btn-success m-3">
+        Add Product Color Size
+    </a>
+
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Size</th>
+                <th>Quantity</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($this->viewData['productColorSizes'] as $colorSize) : ?>
+                <tr>
+                    <td><?= $colorSize->id ?></td>
+                    <td><?= $colorSize->size_name ?></td>
+                    <td><?= $colorSize->qty ?></td>
+                    <td>
+                        <a href="/product/adminColorSizeEdit/<?= $colorSize->id ?>" class="btn btn-warning">Edit</a>
+                        <a href="/product/adminColorSizeDelete/<?= $colorSize->id ?>" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<?php
+
+include VIEW . 'admin/layouts/footer.php';
