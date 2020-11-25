e7411f24fe3d024edc3e4b47adfa87b53c58b8fb
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-24 22:39:35 -0800

    Add AdminColorList in ProductContorooler.php

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -17,6 +17,8 @@ class ProductController extends Controller
         $this->view->render();
     }
 
+
+
     public function adminList()
     {
         if (!Session::isAdmin()) {
@@ -117,4 +119,174 @@ class ProductController extends Controller
         $this->go('product', 'adminList');
 
     }
+
+
+
+    public function adminColorList($id = 0)
+    {
+        if (!Session::isAdmin() || $id == 0) {
+            $this->go('home', 'auth');
+        }
+
+        $productColorModel = $this->model('ProductColor');
+        $productData['productColors'] = $productColorModel->getByProductId($id);
+
+        $productModel = $this->model('Product');
+        $productData['product'] = $productModel->getById($id);
+
+        $this->setView('admin/productColorsList', $productData);
+        $this->view->pageTitle = SITENAME . " - Product Color List";
+        $this->view->render();
+
+    }
+
+    public function adminColorAdd($id = 0)
+    {
+        if (!Session::isAdmin() || $id == 0) {
+            $this->go('home', 'auth');
+        }
+
+        $colorModel = $this->model('Color');
+        $data['colors'] = $colorModel->getAll();
+
+        $productModel = $this->model('Product');
+        $data['product'] = $productModel->getById($id);
+
+        $this->setView('admin/productColorAdd', $data);
+        $this->view->pageTitle = SITENAME . " - Product Color Add";
+        $this->view->render();
+    }
+
+    public function adminDoColorAdd()
+    {
+        if (!Session::isAdmin()) {
+            $this->go('home', 'auth');
+        }
+
+        if (!isset($_POST['product_id']) || !isset($_POST['color_id'])
+            || empty($_POST['product_id']) || empty($_POST['color_id'])) {
+            $_SESSION['err'] = 'All fields should be filled';
+            $this->go('product', 'adminList' );
+        }
+
+        $productColorModel = $this->model('ProductColor');
+
+        if ($productColorModel->add($_POST['product_id'], $_POST['color_id'])) {
+            $_SESSION['success'] = true;
+            $this->go('product', 'adminColorList', $_POST['product_id']);
+        }
+    }
+
+    public function adminColorDelete($id = 0)
+    {
+        if (!Session::isAdmin() || $id == 0 ) {
+            $this->go('home', 'auth');
+        }
+
+        $productColorModel = $this->model('ProductColor');
+        $productColorModel->delete($id);
+        $this->goBack();
+    }
+
+
+
+    public function adminColorSizeList($id = 0)
+    {
+        if (!Session::isAdmin() || $id == 0) {
+            $this->go('home', 'auth');
+        }
+
+        $productColorSizeModel = $this->model('ProductColorSize');
+        $productData['productColorSizes'] = $productColorSizeModel->getByProductColorId($id);
+
+        $productColorModel = $this->model('ProductColor');
+        $productData['productColor'] = $productColorModel->getById($id);
+
+        $this->setView('admin/productColorSizesList', $productData);
+        $this->view->pageTitle = SITENAME . " - Product Color Size List";
+        $this->view->render();
+    }
+
+    public function adminColorSizeAdd($id = 0)
+    {
+        if (!Session::isAdmin() || $id == 0) {
+            $this->go('home', 'auth');
+        }
+
+        $productColorModel = $this->model('ProductColor');
+        $data['productColor'] = $productColorModel->getById($id);
+
+        $sizeModel = $this->model('Size');
+        $data['sizes'] = $sizeModel->getAll();
+
+        $this->setView('admin/productColorSizeAdd', $data);
+        $this->view->pageTitle = SITENAME . " - Product Color Size Add";
+        $this->view->render();
+    }
+
+    public function adminDoColorSizeAdd()
+    {
+        if (!Session::isAdmin()) {
+            $this->go('home', 'auth');
+        }
+
+        if (!isset($_POST['product_color_id']) || !isset($_POST['size_id']) || !isset($_POST['qty'])
+            || empty($_POST['product_color_id']) || empty($_POST['size_id']) || empty($_POST['qty'])) {
+            $_SESSION['err'] = 'All fields should be filled';
+            $this->goBack();
+        }
+
+        $productColorSize = $this->model('ProductColorSize');
+
+        if ($productColorSize->add($_POST['product_color_id'], $_POST['size_id'], $_POST['qty'])) {
+            $_SESSION['success'] = true;
+            $this->go('product', 'adminColorSizeList', $_POST['product_color_id']);
+        }
+    }
+
+    public function adminColorSizeEdit($id = 0)
+    {
+        if (!Session::isAdmin() || $id == 0) {
+            $this->go('home', 'auth');
+        }
+
+        $productColorSizeModel = $this->model('ProductColorSize');
+        $data['productColorSize'] = $productColorSizeModel->getById($id);
+
+        $this->setView('admin/productColorSizeEdit', $data);
+        $this->view->pageTitle = SITENAME . " - Product Color Size Edit";
+        $this->view->render();
+    }
+
+    public function adminDoColorSizeEdit()
+    {
+        if (!Session::isAdmin()) {
+            $this->go('home', 'auth');
+        }
+
+        if (!isset($_POST['product_color_size_id']) || !isset($_POST['qty'])
+            || empty($_POST['product_color_size_id']) || empty($_POST['qty'])) {
+            $_SESSION['err'] = 'All fields should be filled';
+            $this->goBack();
+        }
+
+        $productColorSizeModel = $this->model('ProductColorSize');
+
+        if ($productColorSizeModel->edit($_POST['product_color_size_id'], $_POST['qty'])) {
+            $_SESSION['success'] = true;
+            $this->go('product', 'adminColorSizeList', $_POST['product_color_id']);
+        }
+    }
+
+    public function adminColorSizeDelete($id = 0)
+    {
+        if (!Session::isAdmin() || $id == 0 ) {
+            $this->go('home', 'auth');
+        }
+
+        $productColorSizeModel = $this->model('ProductColorSize');
+        $productColorSizeModel->delete($id);
+        $this->goBack();
+    }
+
 }
