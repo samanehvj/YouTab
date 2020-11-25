9918640f4622f3df94bcc7d3184f65296cb628c4
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-25 02:51:14 -0800

    Create productColorImageList view

---
+++ app/views/admin/productColorImagesList.php
@@ -0,0 +1,43 @@
+<?php
+
+include VIEW . 'admin/layouts/header.php';
+include VIEW . 'admin/layouts/sidebar.php';
+
+?>
+<div class="table-responsive">
+    <h3 class="text-center my-3">
+        <?= $this->viewData['productColor']->product_name . " : (" . $this->viewData['productColor']->color_name .")" ?>
+    </h3>
+
+    <a href="/product/adminColorImageAdd/<?= $this->viewData['productColor']->id ?>" class="btn btn-success m-3">
+        Add Product Color Image
+    </a>
+
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Image</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($this->viewData['productColorImages'] as $img) : ?>
+                <tr>
+                    <td><?= $img->id ?></td>
+                    <td>
+                        <a href="<?= URLROOT . '/' . $img->img ?>" target="_blank">
+                            <img src="<?= URLROOT . '/' . $img->img ?>" height="100px" >
+                        </a>
+                    </td>
+                    <td>
+                        <a href="/product/adminColorImageDelete/<?= $img->id ?>" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<?php
+
+include VIEW . 'admin/layouts/footer.php';
