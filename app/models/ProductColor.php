b15b3472f759360f9f91139236632dc92fbe8c8c
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-25 02:44:47 -0800

    Update ProductColor model to get product category name

--- app/models/ProductColor.php
+++ app/models/ProductColor.php
@@ -39,12 +39,14 @@ Class ProductColor {
 
     public function getById($id)
     {
-        $sql = "SELECT pc.*, p.name 'product_name', c.name 'color_name', c.hex 'color_hex'
+        $sql = "SELECT pc.*, p.name 'product_name', c.name 'color_name', c.hex 'color_hex', cat.name 'category_name'
                 FROM product_colors AS pc
                 JOIN colors AS c 
                 ON c.id=pc.color_id
                 JOIN products AS p 
                 ON p.id=pc.product_id
+                JOIN categories AS cat
+                ON p.category_id=cat.id
                 WHERE pc.id = :id";
         $this->db->query($sql);
         $this->db->bind(':id', $id);
