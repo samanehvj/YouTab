5de840a979aea73e22b0d7c9fa943a38f069fe9f
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-26 15:59:33 -0800

    Add product_detail from DB

--- app/views/product/product_detail.php
+++ app/views/product/product_detail.php
@@ -4,52 +4,71 @@
 
         <div class="row my-5">
 
-
             <div class="col-lg-2 order-2 order-lg-1 d-flex product-detail-thumbs ">
-                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
-                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
-                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
-                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
-                <img width="100%" height="auto" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656" alt="">
+
+                <?php foreach ($this->viewData->colors[0]->images as $img): ?>
+
+                <img width="100%" height="auto" src="/<?= $img->img ?>" alt="">
+
+                <?php endforeach; ?>
 
             </div>
+
+
             <div class=" col-lg-6 order-1 order-lg-">
                 <div class="larg-cat d-flex flex-column justify-content-between h-100 mb-3 mb-lg-0 p-2  ">
-                    <img class="img-fluid w-100" src="https://ca.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-since-1854-leather-insert-technical-gabardine-fit-and-flare-dress-ready-to-wear--FKDR35P89003_PM2_Front%20view.png?wid=656&hei=656">
+                    <img class="img-fluid w-100" src="/<?= $this->viewData->colors[0]->images[0]->img ?>">
                 </div>
             </div>
 
 
             <div class="col-lg-4 order-3 ">
 
-
                 <div class="card" >
                     <div class="card-body">
-                        <h3 class="card-title">Leather Dress</h3>
-                        <h5>$3500</h5>
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Size</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>X-Small</option>
-                                <option>Small</option>
-                                <option>Medium</option>
-                                <option>large</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Color</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>Navy</option>
-                                <option>White</option>
+                        <h3 class="card-title"><?= $this->viewData->name ?></h3>
+                        <h5>$<?= $this->viewData->price ?></h5>
 
-                            </select>
-                        </div>
-                        <p>Sleek and structured, this sleeveless fit-and-flare dress is cut from technical gabardine with tone-on-tone lining. Leather inserts create a sharp, graphic look and nod to YouTab heritage of craftsmanship. A statement zipper runs the length of the body and a jewel-like metallic buckle adds a statement feature.</p>
+                        <form action="/product/addToCart" method="post">
 
+                            <input type="hidden" name="product_id" value="<?= $this->viewData->id ?>" />
+                            <input type="hidden" name="product_name" value="<?= $this->viewData->name ?>" />
+                            <input type="hidden" name="product_price" value="<?= $this->viewData->price ?>" />
+                            <input type="hidden" name="product_image"
+                                   value="<?= $this->viewData->colors[0]->images[0]->img ?>"
+                            />
 
-                        <div class="text-center mt-5">
-                            <a href="products/category/1" class="btn btn-dark text-center w-50 "> Place to Cart  </a>
-                        </div>
+                            <div class="form-group">
+                                <label for="size">Size</label>
+                                <select name="product_color_size" class="form-control" id="size">
+
+                                    <?php  foreach($this->viewData->colors[0]->sizes as $size): ?>
+                                        <option value="<?= $size->id . "," . $size->size_name ?>">
+                                            <?= $size->size_name ?>
+                                        </option>
+                                    <?php endforeach; ?>
+
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="color">Color</label>
+                                <select name="product_color" class="form-control" id="color">
+
+                                    <?php  foreach($this->viewData->colors as $color): ?>
+                                        <option value="<?= $color->id . "," . $color->name ?>">
+                                            <?= $color->name ?>
+                                        </option>
+                                    <?php endforeach; ?>
+
+                                </select>
+                            </div>
+
+                            <div class="text-center mt-5">
+                                <button type="submit" class="btn btn-dark text-center w-50 "> Place to Cart  </button>
+                            </div>
+
+                        </form>
                     </div>
                 </div>
 
