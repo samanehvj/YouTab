72defd293467eb4430ce7f5b086d5fd4ff92f3e3
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-25 23:02:00 -0800

    Create static categories  from DB

--- app/views/index.php
+++ app/views/index.php
@@ -1,9 +1,5 @@
 <?php include VIEW . 'layouts/header.php' ?>
 
-
-
-
-
     <!--start hero image -->
     <div class="container-fluid  ">
         <img src="/imgs/hero-home.jpg" class="img-fluid w-100  ">
@@ -17,30 +13,29 @@
         <div class="row my-5 text-home"> <!--row -->
             <div class=" col-lg-6"> <!--start left cat-->
                 <div class="larg-cat d-flex flex-column justify-content-between h-100 mb-3 mb-lg-0 p-2 border border-dark">
-                    <img class="img-fluid w-100" src="/imgs/skirt-cat.jpg">
-                    <h1 class="text-center my-3 home-titile">Skirts</h1>
+                    <img class="img-fluid mh-50 w-100" src="<?= $this->viewData['topCategories'][2]->img ?> ">
+                    <h1 class="text-center my-3 home-titile"><?= $this->viewData['topCategories'][2]->name ?></h1>
                     <p class="py-3 px-5 text-center text-lg-left text-cat">
-                        Our fabulous Fall/Winter collection has arrived and it’s better than ever. We’ve taken some inspiration from the key global trends and put our own stamp on them. By focusing on subtle and chic looks, we’ve produced relaxed designs that are as stylish as they are comfortable.
-
-                                   </p>
+                        <?= $this->viewData['topCategories'][2]->info ?>
+                    </p>
                     <div class="text-center mt-5">
-                        <a href="products/category/1" class="btn btn-dark  text-center collection">View collection </a>
+                        <a href="products/category/<?= $this->viewData['topCategories'][2]->id ?>" class="btn btn-dark  text-center collection">View collection </a>
                     </div>
                 </div>
             </div> <!--end left cat-->
             <div class=" col-lg-6 d-flex flex-column justify-content-between">  <!--start right cat-->
                 <div class="col-12 small-cat mb-4 p-2 border border-dark">
-                    <img class="img-fluid w-100" src="/imgs/dress-cat.jpg">
-                    <h1 class="text-center my-3">Dresses</h1>
+                    <img class="img-fluid w-100" src="<?= $this->viewData['topCategories'][1]->img ?>">
+                    <h1 class="text-center my-3"><?= $this->viewData['topCategories'][1]->name ?></h1>
                     <div class="text-center">
-                        <a href="products/category/1" class="btn btn-dark text-center">View collection </a>
+                        <a href="products/category/<?= $this->viewData['topCategories'][0]->id ?>" class="btn btn-dark text-center">View collection </a>
                     </div>
                 </div>
                 <div class="col-12 small-cat p-2 border border-dark">
-                    <img class="img-fluid w-100" src="/imgs/top-cat.jpg">
-                    <h1 class="text-center my-3">Tops</h1>
+                    <img class="img-fluid w-100" src="<?= $this->viewData['topCategories'][0]->img ?>">
+                    <h1 class="text-center my-3"><?= $this->viewData['topCategories'][0]->name ?></h1>
                     <div class="text-center btn-cat">
-                        <a href="products/category/1" class="btn btn-dark text-center">View collection </a>
+                        <a href="products/category/<?= $this->viewData['topCategories'][0]->id ?>" class="btn btn-dark text-center">View collection </a>
                     </div>
                 </div>
             </div> <!--end right-top cat-->
@@ -54,7 +49,7 @@
             <div class="row my-5"> <!-- start row of Most famous product-->
                 <div class=" col-lg-6"> <!-- start row of dress left side  famous product-->
                     <div class="larg-cat d-flex flex-column justify-content-between h-100 mb-3 mb-lg-0 p-2  ">
-                        <img class="img-fluid w-100" src="/imgs/dresses/4.png">
+                        <img class="img-fluid w-100" src="<?= $this->viewData['topProducts'][0]->img ?>">
                     </div>
                 </div> <!-- end row of dress left side  famous product-->
 
@@ -62,12 +57,12 @@
 
                     <div class=" col-lg-6 ">  <!--start right cat-->
                         <div class="col-12 small-cat mb-4 p-2 k">
-                            <img class="img-fluid w-100" src="/imgs/skirts/18.png">
+                            <img class="img-fluid w-100" src="<?= $this->viewData['topProducts'][1]->img ?>">
                             <h5 class="products-text mx-4 my-2">
-                                Skirt with Metallic
+                                <?= $this->viewData['topProducts'][1]->name ?>
                             </h5>
                             <h5 class="mx-4 my-2">
-                                $2000
+                                $<?= $this->viewData['topProducts'][1]->price ?>
                             </h5>
                         </div>
                     </div>
@@ -75,12 +70,12 @@
 
                     <div class=" col-lg-6 ">  <!--start right cat-->
                         <div class="col-12 small-cat mb-4 p-2 k">
-                            <img class="img-fluid w-100" src="/imgs/tops/4.png">
+                            <img class="img-fluid w-100" src="<?= $this->viewData['topProducts'][2]->img ?>">
                             <h5 class="products-text mx-4 my-2">
-                                Sleeveless Shirt
+                                <?= $this->viewData['topProducts'][2]->name ?>
                             </h5>
                             <h5 class="mx-4 my-2">
-                                $2000
+                                $<?= $this->viewData['topProducts'][2]->price ?>
                             </h5>
                         </div>
                     </div>
@@ -89,11 +84,12 @@
 
                     <div class=" col-lg-6 ">  <!--start right cat-->
                         <div class="col-12 small-cat mb-4 p-2 k">
-                            <img class="img-fluid w-100" src="/imgs/tops/14.png">
+                            <img class="img-fluid w-100" src="<?= $this->viewData['topProducts'][3]->img ?>">
                             <h5 class="products-text mx-4 my-2">
-                                Short-Sleeeved T-Shirt                            </h5>
+                                <?= $this->viewData['topProducts'][3]->name ?>
+                            </h5>
                             <h5 class="mx-4 my-2">
-                                $2000
+                                $<?= $this->viewData['topProducts'][3]->price ?>
                             </h5>
                         </div>
                     </div>
@@ -101,11 +97,12 @@
 
                     <div class=" col-lg-6 ">  <!--start right cat-->
                         <div class="col-12 small-cat mb-4 p-2 k">
-                            <img class="img-fluid w-100" src="/imgs/skirts/4.jpg">
+                            <img class="img-fluid w-100" src="<?= $this->viewData['topProducts'][4]->img ?>">
                             <h5 class="products-text mx-4 my-2">
-                                A-Line Mini Skirt                             </h5>
+                                <?= $this->viewData['topProducts'][4]->name ?>
+                            </h5>
                             <h5 class="mx-4 my-2">
-                                $2000
+                                $<?= $this->viewData['topProducts'][4]->price ?>
                             </h5>
                         </div>
                     </div>
