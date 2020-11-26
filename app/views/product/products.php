486da14eea0e065029b73c1cc457a00a321eaeca
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-25 20:06:55 -0800

    Create Product.php

--- app/views/product/products.php
+++ app/views/product/products.php
@@ -1,167 +1,37 @@
 <?php include VIEW . 'layouts/header.php' ?>
 
-   <div class="container"> <!--start container-->
+    <div class="container"> <!--start container-->
 
-       <div class="row my-5 product-title"> <!--start row-->
+        <div class="row my-5 product-title"> <!--start row-->
 
-           <!-- start row 1  dress-->
+            <!-- start row 1  dress-->
 
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
-               <a href="/product/productDetail">
-                   <img src="/imgs/dresses/8.png" class="img-fluid col-12">
-               </a>
-               <div class="col-12 mt-2 ">
-               <div class="row">
-                   <div class="col-7">
-                       <a href="/product/productDetail">Fit-And-Flare Dress </a><br/>
-                       <span>$2900</span>
-                   </div>
-                   <div class="col-4 my-2">
-                       <span class="py-2 px-3 ml-1 bg-light border border-dark"> </span>
-                       <span class="py-2 px-3 ml-1 bg-dark"> </span>
-                   </div>
-           </div>
-               </div>
-           </div>
+            <?php foreach ($this->viewData['products'] as $product): ?>
 
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <a href="/product/productDetail">
-                   <img src="/imgs/dresses/17.png" class="img-fluid col-12">
+            <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
+               <a href="/product/productDetail/<?= $product->id ?>">
+                   <img src="/<?= $product->img ?>" class="img-fluid col-12">
                </a>
                <div class="col-12 mt-2 ">
                    <div class="row">
                        <div class="col-7">
-                           <a href="/product/productDetail">Floral  Mini-Dress </a><br/>
-                           <span>$2900</span>
-                       </div>
-
-                   </div>
-               </div>
-           </div>
-
-
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <a href="/product/productDetail">
-                   <img src="/imgs/dresses/4.png" class="img-fluid col-12">
-               </a>
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7">
-                           <a href="/product/productDetail">Leather dress</a><br/>
-                           <span>$2900</span>
-                       </div>
-                   </div>
-               </div>
-           </div>
-
-           <!-- end row 1  dress-->
-
-
-
-
-           <!-- start row 2  top-->
-
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
-               <img src="/imgs/tops/2.png" class="img-fluid col-12">
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7 ">
-                           <a href="">Sleeveless Shirt </a><br/>
-                           <span>$2900</span>
+                           <a href="/product/productDetail/<?= $product->id ?>"> <?= $product->name ?> </a><br/>
+                           <span>$<?= $product->price ?></span>
                        </div>
                        <div class="col-4 my-2">
-                           <span class="py-2 px-3 ml-1 bg-light border border-dark"> </span>
-                           <span class="py-2 px-3 ml-1 bg-dark"> </span>
+                           <?php  foreach ($product->colors as $pc): ?>
+
+                                <span class="py-1 px-2 ml-1 border border-dark"
+                                      style="background-color:#<?= $pc->hex ?> ">
+                                </span>
+
+                            <?php endforeach; ?>
                        </div>
-                   </div>
+                    </div>
                </div>
            </div>
 
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <img src="/imgs/tops/12.png" class="img-fluid col-12">
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7 ">
-                           <a href="">Short-Sleeeved </a><br/>
-                           <span>$2900</span>
-                       </div>
-
-                   </div>
-               </div>
-           </div>
-
-
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <img src="/imgs/tops/17.png" class="img-fluid col-12">
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7">
-                           <a href="">Side Strap T-Shirt </a><br/>
-                           <span>$2900</span>
-                       </div>
-                   </div>
-               </div>
-           </div>
-
-           <!-- end row 2  top-->
-
-
-
-
-           <!-- start row 3  skirt-->
-
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center">
-               <img src="/imgs/skirts/17.png" class="img-fluid col-12">
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7 ">
-                           <a href="">Skirt with Metallic </a><br/>
-                           <span>$2900</span>
-                       </div>
-                       <div class="col-4 my-2">
-                           <span class="py-2 px-3 ml-1 bg-danger"> </span>
-                           <span class="py-2 px-3 ml-1 bg-dark"> </span>
-                       </div>
-                   </div>
-               </div>
-           </div>
-
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <img src="/imgs/skirts/3.jpg" class="img-fluid col-12">
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7 ">
-                           <a href="">A-Line Mini Skirt </a><br/>
-                           <span>$2900</span>
-                       </div>
-
-                   </div>
-               </div>
-           </div>
-
-
-           <div class="col-md-6 mt-2 col-lg-4 d-flex flex-column justify-contern-center" >
-               <img src="/imgs/skirts/7.png" class="img-fluid col-12">
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7">
-                           <a href="">Monogram Mini Skirt </a><br/>
-                           <span>$2900</span>
-                       </div>
-
-                   </div>
-               </div>
-           </div>
-
-           <!-- end row 3  skirt-->
-
+            <?php endforeach; ?>
 
        </div> <!--end row-->
 
