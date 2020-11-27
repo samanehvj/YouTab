7ed52243b0fc8e5f349c91e7342a30f195d3f0f9
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-27 04:20:49 -0800

    Create orderEdit.php

---
+++ app/views/admin/orderEdit.php
@@ -0,0 +1,34 @@
+<?php
+
+include VIEW . 'admin/layouts/header.php';
+include VIEW . 'admin/layouts/sidebar.php';
+
+?>
+<div class="container d-flex justify-content-around ">
+    <form class="mt-5" method="post" action="/order/adminDoEdit" >
+    <h1 class="text-center m-5">Order Number: <?= $this->viewData->id ?></h1>
+        <input type="hidden" name="id" value="<?= $this->viewData->id ?>" />
+
+        <div class="form-group">
+            <select class="form-control" id="color" name="size_id" required>
+                <option value="">Set Delivery Status</option>
+                <option <?= $this->viewData->is_delivered ? '' : 'selected' ?> value="0">Pending</option>
+                <option <?= $this->viewData->is_delivered ? 'selected' : '' ?> value="1">Delivered</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <select class="form-control" id="color" name="size_id" required>
+                <option value="">Set Order Status</option>
+                <option <?= $this->viewData->is_canceled ? 'selected' : '' ?> value="0">Canceled</option>
+                <option <?= $this->viewData->is_canceled ? '' : 'selected' ?> value="1">Active</option>
+            </select>
+        </div>
+
+        <button type="submit" class="btn btn-warning mt-3">Edit</button>
+
+    </form>
+</div>
+<?php
+
+include VIEW . 'admin/layouts/footer.php';
