e0c880f02412a7a6d1ee39c7d547db9788372476
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-26 16:00:01 -0800

    Add product from DB

--- app/views/product/products.php
+++ app/views/product/products.php
@@ -12,28 +12,29 @@
                <a href="/product/productDetail/<?= $product->id ?>">
                    <img src="/<?= $product->img ?>" class="img-fluid col-12">
                </a>
-               <div class="col-12 mt-2 ">
-                   <div class="row">
-                       <div class="col-7">
-                           <a href="/product/productDetail/<?= $product->id ?>"> <?= $product->name ?> </a><br/>
-                           <span>$<?= $product->price ?></span>
-                       </div>
-                       <div class="col-4 my-2">
-                           <?php  foreach ($product->colors as $pc): ?>
+                <div class="row mt-2">
+                   <div class="col-7">
+                       <a href="/product/productDetail/<?= $product->id ?>"> <?= $product->name ?> </a><br/>
+                       <span>$<?= $product->price ?></span>
+                   </div>
+                   <div class="col-4 my-2">
+                       <?php  foreach ($product->colors as $pc): ?>
 
-                                <span class="py-1 px-2 ml-1 border border-dark"
-                                      style="background-color:#<?= $pc->hex ?> ">
-                                </span>
+                            <span class="py-1 px-2 ml-1 border border-dark"
+                                  style="background-color:#<?= $pc->hex ?> ">
+                            </span>
 
-                            <?php endforeach; ?>
-                       </div>
-                    </div>
-               </div>
-           </div>
+                        <?php endforeach; ?>
+                   </div>
+                </div>
+            </div>
 
             <?php endforeach; ?>
 
-       </div> <!--end row-->
+
+        </div> <!--end row-->
+
+        <div class="clearfix"></div>
 
    </div> <!--end container-->
 
