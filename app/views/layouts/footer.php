3f995ea4057da1b0400711679ff3433effac1930
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-12-10 00:03:04 -0800

    Edit footer

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -3,7 +3,8 @@
 <!--footer start-->
 <footer class="footer bg-dark py-2">
     <div class="d-flex flex-wrap justify-content-around">
-        <span class="text-light footer-text "> © YouTab 2020. All rights reserved.</span>
+        <span class="text-light footer-text "> © YouTab 2020. All rights reserved <a href="www.samanehvj.com">Samaneh Vajdi</a>
+.</span>
         <span class="d-inline-block bg-dark text-light">
                     <a class="p-1 h4" href=""><i class="fab fa-facebook-square text-light"></i></a>
                     <a class="p-1 h4" href=""> <i class="fab fa-instagram-square text-light"></i></a>
